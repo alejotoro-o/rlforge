5e240599ed0d669522d962709dcff0d8759a9c6c
Alejandro Toro-Ossaba <alejotoro.o@gmail.com>
2023-12-23 13:55:26 -0500

    Update docs config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,7 @@ release = '0.0.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinxawesome_theme.highlighting',
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
